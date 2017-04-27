040f2ffe9638e6f61fd655ba4a8c35c196d763fe
Sven Erik Vinkemeier <svenvinkemeier@live.de>
2017-04-27 15:07:35 +0200

    Added swagger for testing purposes

--- HiP-DataStore/Startup.cs
+++ HiP-DataStore/Startup.cs
@@ -4,11 +4,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using PaderbornUniversity.SILab.Hip.DataStore.Core;
+using PaderbornUniversity.SILab.Hip.Webservice;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace PaderbornUniversity.SILab.Hip.DataStore
 {
     public class Startup
     {
+        private const string _Version = "v1";
+        private const string _Name = "HiP Data Store API";
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -24,7 +29,14 @@ namespace PaderbornUniversity.SILab.Hip.DataStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            // Register the Swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                // Define a Swagger document
+                c.SwaggerDoc("v1", new Info { Title = _Name, Version = _Version });
+                c.OperationFilter<SwaggerOperationFilter>();
+            });
+
             services.AddMvc();
             services.AddSingleton<EventStoreClient>();
             services.AddSingleton<CacheDatabaseManager>();
@@ -37,6 +49,22 @@ namespace PaderbornUniversity.SILab.Hip.DataStore
             loggerFactory.AddDebug();
 
             app.UseMvc();
+
+            // Swagger / Swashbuckle configuration:
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger(c =>
+            {
+                c.PreSerializeFilters.Add((swaggerDoc, httpReq) => swaggerDoc.Host = httpReq.Host.Value);
+            });
+
+            // Configure SwaggerUI endpoint
+            app.UseSwaggerUI(c =>
+            {
+                // TODO: Only a hack, if HiP-Swagger is running, SwaggerUI can be disabled for Production
+                c.SwaggerEndpoint((env.IsDevelopment() ? "/swagger" : "..") +
+                                  "/" + _Version + "/swagger.json", _Name + _Version);
+            });
         }
     }
 }
