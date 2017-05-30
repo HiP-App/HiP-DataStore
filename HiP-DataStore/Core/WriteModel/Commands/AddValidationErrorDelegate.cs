d5657cd011c97d33309522e1d126a66171ed64c6
Sven Erik Vinkemeier <svenvinkemeier@live.de>
2017-05-30 13:14:38 +0200

    Added delegate documentation

--- HiP-DataStore/Core/WriteModel/Commands/AddValidationErrorDelegate.cs
+++ HiP-DataStore/Core/WriteModel/Commands/AddValidationErrorDelegate.cs
@@ -1,4 +1,10 @@
 ﻿namespace PaderbornUniversity.SILab.Hip.DataStore.Core.WriteModel.Commands
 {
+    /// <summary>
+    /// The signature of a method that adds a validation error message to a collection of error messages, such as a
+    /// <see cref="Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary"/>.
+    /// </summary>
+    /// <param name="key">The name of the property of which the validation failed</param>
+    /// <param name="errorMessage">A description of the error</param>
     public delegate void AddValidationErrorDelegate(string key, string errorMessage);
 }
