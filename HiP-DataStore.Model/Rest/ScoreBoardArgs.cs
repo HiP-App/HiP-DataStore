dd411f9c680c6de8a11428e3691ab02fa641aade
Timur <musaevt@yahoo.com>
2017-07-24 15:34:27 +0200

    Added Length Parameter to Call (#48)
    API/ScoreBoard

---
+++ HiP-DataStore.Model/Rest/ScoreBoardArgs.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaderbornUniversity.SILab.Hip.DataStore.Model.Rest
+{
+    public class ScoreBoardArgs
+    {
+        [Range(1,int.MaxValue)]
+        public int? Length { get; set; }
+    }
+}
