431d736da1de0cd873d7fb5b7bce367e3d31543e
Sven Vinkemeier <svenvinkemeier@live.de>
2017-10-05 21:04:34 +0200

    Added history APIs for other resource types

--- HiP-DataStore/Controllers/HistoryController.cs
+++ HiP-DataStore/Controllers/HistoryController.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PaderbornUniversity.SILab.Hip.DataStore.Core;
 using PaderbornUniversity.SILab.Hip.DataStore.Model;
 using PaderbornUniversity.SILab.Hip.DataStore.Model.Entity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace PaderbornUniversity.SILab.Hip.DataStore.Controllers
@@ -12,28 +11,52 @@ namespace PaderbornUniversity.SILab.Hip.DataStore.Controllers
     /// <summary>
     /// Provides methods to obtain a version history or specific versions of an entity.
     /// </summary>
+    [Authorize]
     public class HistoryController : Controller
     {
         private readonly EventStoreClient _eventStore;
 
-        protected HistoryController(EventStoreClient eventStore)
-        {
-            _eventStore = eventStore;
-        }
+        public HistoryController(EventStoreClient eventStore) => _eventStore = eventStore;
 
         [HttpGet("/api/Exhibits/{id}/History")]
-        public Task<IActionResult> GetExhibitSummary(int id) => GetSummaryAsync(ResourceType.Exhibit, id);
+        public Task<IActionResult> GetExhibitSummary(int id) =>
+            GetSummaryAsync(ResourceType.Exhibit, id);
 
-        [HttpGet("/api/Exhibits/{id}/History")]
-        public Task<IActionResult> GetExhibitVersion(int id) => GetSummaryAsync(ResourceType.Exhibit, id);
+        [HttpGet("/api/Exhibits/{id}/History/{timestamp}")]
+        public Task<IActionResult> GetExhibitVersion(int id, DateTimeOffset timestamp) =>
+            GetVersionAsync<Exhibit>(ResourceType.Exhibit, id, timestamp);
 
         [HttpGet("/api/Exhibits/Pages/{id}/History")]
-        public Task<IActionResult> GetExhibitPageSummary(int id) => GetSummaryAsync(ResourceType.ExhibitPage, id);
+        public Task<IActionResult> GetExhibitPageSummary(int id) =>
+            GetSummaryAsync(ResourceType.ExhibitPage, id);
 
-        [HttpGet("/api/Exhibits/Pages/{id}/History")]
-        public Task<IActionResult> GetExhibitPageVersion(int id) => GetSummaryAsync(ResourceType.ExhibitPage, id);
+        [HttpGet("/api/Exhibits/Pages/{id}/History/{timestamp}")]
+        public Task<IActionResult> GetExhibitPageVersion(int id, DateTimeOffset timestamp) =>
+            GetVersionAsync<ExhibitPage>(ResourceType.ExhibitPage, id, timestamp);
 
-        // TODO: Implement for all resource types
+        [HttpGet("/api/Media/{id}/History")]
+        public Task<IActionResult> GetMediaSummary(int id) =>
+            GetSummaryAsync(ResourceType.Media, id);
+
+        [HttpGet("/api/Media/{id}/History/{timestamp}")]
+        public Task<IActionResult> GetMediaVersion(int id, DateTimeOffset timestamp) =>
+            GetVersionAsync<MediaElement>(ResourceType.Media, id, timestamp);
+
+        [HttpGet("/api/Routes/{id}/History")]
+        public Task<IActionResult> GetRouteSummary(int id) =>
+            GetSummaryAsync(ResourceType.Route, id);
+
+        [HttpGet("/api/Routes/{id}/History/{timestamp}")]
+        public Task<IActionResult> GetRouteVersion(int id, DateTimeOffset timestamp) =>
+            GetVersionAsync<Route>(ResourceType.Route, id, timestamp);
+
+        [HttpGet("/api/Tags/{id}/History")]
+        public Task<IActionResult> GetTagSummary(int id) =>
+            GetSummaryAsync(ResourceType.Tag, id);
+
+        [HttpGet("/api/Tags/{id}/History/{timestamp}")]
+        public Task<IActionResult> GetTagVersion(int id, DateTimeOffset timestamp) =>
+            GetVersionAsync<Route>(ResourceType.Tag, id, timestamp);
 
 
         private async Task<IActionResult> GetSummaryAsync(ResourceType type, int id)
@@ -46,7 +69,6 @@ namespace PaderbornUniversity.SILab.Hip.DataStore.Controllers
             return Ok(summary);
         }
 
-        [HttpGet("{id}/History/{timestamp}")]
         private async Task<IActionResult> GetVersionAsync<T>(ResourceType type, int id, DateTimeOffset timestamp)
             where T : ContentBase
         {
