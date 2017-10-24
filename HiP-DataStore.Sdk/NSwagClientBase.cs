a4bd557e912638db17470d199f1ea6cc96a1f5c5
Sven Vinkemeier <svenvinkemeier@live.de>
2017-10-25 00:02:32 +0200

    Added NSwag spec & code generation

---
+++ HiP-DataStore.Sdk/NSwagClientBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PaderbornUniversity.SILab.Hip.DataStore
+{
+    public abstract class NSwagClientBase
+    {
+        /// <summary>
+        /// The value for the HTTP Authorization header,
+        /// e.g. "Bearer [Your JWT token here]".
+        /// </summary>
+        public string Authorization { get; set; }
+
+        protected Task<HttpClient> CreateHttpClientAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpClient
+            {
+                DefaultRequestHeaders = { { "Authorization", Authorization } }
+            });
+        }
+    }
+}
