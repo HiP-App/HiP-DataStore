96ba1f4b60b62a875408dff33b8880fb73b96249
Timur <musaevt@yahoo.com>
2017-11-24 11:37:51 +0100

    Changed rating API to report the number of ratings given for each rating value

--- HiP-DataStore.Model/Rest/RatingResult.cs
+++ HiP-DataStore.Model/Rest/RatingResult.cs
@@ -1,12 +1,15 @@
-﻿namespace PaderbornUniversity.SILab.Hip.DataStore.Model.Rest
+﻿using System.Collections.Generic;
+
+namespace PaderbornUniversity.SILab.Hip.DataStore.Model.Rest
 {
     public class RatingResult
     {
         public int Id { get; set; }
-              
+
         public double Average { get; set; }
 
         public int Count { get; set; }
 
+        public Dictionary<int,int> RatingTable { get; set; }
     }
 }
