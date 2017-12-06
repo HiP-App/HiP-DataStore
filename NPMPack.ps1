2dc550e50e0b3942f9f79cd98e93ac61a0e34447
Michél Burkhardt <mburkhardt@live.de>
2017-12-06 15:46:27 +0100

    Fixed NPMPack.ps1 to add tags correctly, modified package.json for new npm version

--- NPMPack.ps1
+++ NPMPack.ps1
@@ -13,14 +13,14 @@ npm install
 Switch ("$env:Build_SourceBranchName") 
 {
     "develop"{		
-		npm --% publish  --registry=%NPMFeed% --tag %tag%	
+		npm --% publish --registry=%NPMFeed% --tag %tag%	
 	}
 
 	"master" {
 		$json = Get-Content -Path package.json | ConvertFrom-Json
 		$env:version = $json.version
 		$env:name = $json.name
-		npm --% dist-tag add %name%@%version% %tag%
+		npm --% dist-tag add %name%@%version% %tag% --registry=%NPMFeed%
 	}
 }
 
