f5d1a0609421dcaeff186ee8d300d9cbe9fb3d1f
Sven Erik Vinkemeier <sven@vinkemeier.de>
2017-12-11 15:19:26 +0100

    Merge into master (#99)
    * Fixed NPMPack.ps1 to add tags correctly, modified package.json for new npm version (#95)
    
    * Fixed: QueryArgs.Status didn't default to "Published" (#97)
    
    * Default values to simplify configuration (#98)
    
    * Added default values for easier configuration
    
    * Updated appsettings.Development.json.example

--- HiP-DataStore/Utility/AuthConfig.cs
+++ HiP-DataStore/Utility/AuthConfig.cs
@@ -2,7 +2,16 @@
 {
     public class AuthConfig
     {
-       public string Audience { get; set; }
-       public string Authority { get; set; }
+        /// <summary>
+        /// Audience of all HiP APIs.
+        /// Default value: "https://hip.cs.upb.de/API"
+        /// </summary>
+        public string Audience { get; set; } = "https://hip.cs.upb.de/API";
+
+        /// <summary>
+        /// Authority.
+        /// Default value: "https://hip.eu.auth0.com/"
+        /// </summary>
+        public string Authority { get; set; } = "https://hip.eu.auth0.com/";
     }
 }
