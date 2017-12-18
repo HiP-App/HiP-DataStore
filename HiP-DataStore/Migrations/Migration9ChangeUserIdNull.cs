6b8e2ed97b375c62b4c0277b13f017bd16f7e460
Timur <musaevt@yahoo.com>
2017-12-18 11:43:47 +0100

    comment change

--- HiP-DataStore/Migrations/Migration9ChangeUserIdNull.cs
+++ HiP-DataStore/Migrations/Migration9ChangeUserIdNull.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace PaderbornUniversity.SILab.Hip.DataStore.Migrations
 {    /// <summary>
-     /// Updating to version 8. We have events in our EventStore that have UserId null. That`s not right. 
+     /// Updating to version 9. We have events in our EventStore that have UserId null. That`s not right. 
      /// In our migration we will replace them with admin userId = auth0|5968ed8cdd1b3733ca94865d
      /// </summary>
     [StreamMigration(from: 8, to: 9)]
