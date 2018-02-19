c5222c21412bb4deab071238c775791c7c2c8d5d
Michél Burkhardt <mburkhardt@live.de>
2018-02-19 17:39:29 +0100

    Removed using

--- HiP-DataStore.Model/Rest/ExhibitQuizQuestionArgs.cs
+++ HiP-DataStore.Model/Rest/ExhibitQuizQuestionArgs.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using PaderbornUniversity.SILab.Hip.DataStore.Model.Utility;
+﻿using PaderbornUniversity.SILab.Hip.DataStore.Model.Utility;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
