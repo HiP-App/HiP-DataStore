752280e80d2b11846dd0e225a27dfd80897f9eba
Michél Burkhardt <mburkhardt@live.de>
2018-03-10 12:05:50 +0100

    Fix migration 11

--- HiP-DataStore/Migrations/Migration11DefaultValues.cs
+++ HiP-DataStore/Migrations/Migration11DefaultValues.cs
@@ -51,27 +51,36 @@ namespace PaderbornUniversity.SILab.Hip.DataStore.Migrations
             while (await events.MoveNextAsync())
             {
                 var current = events.Current;
-                ResourceType resourceType;
 
-                //there are events in the event stream with ResourceType "Quiz" these need to be deleted since a new solution with ResourceType "QuizQuestion" has been implemented
-                try
-                {
-                    resourceType = (current as BaseEvent)?.GetEntityType();
-                    if (resourceType == null) continue;
-                }
-                catch { continue; }
                 switch (current)
                 {
-                    case CreatedEvent ev:
-                        var properties = _dict[(resourceType, ev.Id)];
-                        e.AppendEvent(ev);
-                        foreach (var prop in properties)
+                    case BaseEvent ev:
+                        ResourceType resourceType;
+
+                        //there are events in the event stream with ResourceType "Quiz" these need to be deleted since a new solution with ResourceType "QuizQuestion" has been implemented
+                        try
                         {
-                            if (prop.PropertyType.IsValueType && !(prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>)))
+                            resourceType = (current as BaseEvent)?.GetEntityType();
+                            if (resourceType == null) continue;
+                        }
+                        catch { continue; }
+
+                        if (ev is CreatedEvent)
+                        {
+                            var properties = _dict[(resourceType, ev.Id)];
+                            e.AppendEvent(ev);
+                            foreach (var prop in properties)
                             {
-                                var value = Activator.CreateInstance(prop.PropertyType);
-                                e.AppendEvent(new PropertyChangedEvent(prop.Name, ev.ResourceTypeName, ev.Id, ev.UserId, value));
+                                if (prop.PropertyType.IsValueType && !(prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>)))
+                                {
+                                    var value = Activator.CreateInstance(prop.PropertyType);
+                                    e.AppendEvent(new PropertyChangedEvent(prop.Name, ev.ResourceTypeName, ev.Id, ev.UserId, value));
+                                }
                             }
+                        }
+                        else
+                        {
+                            e.AppendEvent(ev);
                         }
                         break;
                     default:
