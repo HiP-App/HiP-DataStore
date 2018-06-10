15baddbe6b5632fe855278e07910a42317f18299
thielep <thielep@mail.uni-paderborn.de>
2018-06-10 15:42:45 +0200

    Removed switch from NuGetPack script

--- NuGetPack.ps1
+++ NuGetPack.ps1
@@ -1,9 +1,7 @@
 $csproj = (ls *.Sdk\*.csproj).FullName
-Switch ("$env:Build_SourceBranchName")
-{
-    "develop" { dotnet pack "$csproj" -o . }
-    default { exit }
-}
+
+dotnet pack "$csproj" -o . 
+
 $nupkg = (ls *.Sdk\*.nupkg).FullName
 dotnet nuget push "$nupkg" -k "$env:MyGetKey" -s "$env:NuGetFeed"
 $LASTEXITCODE = 0
