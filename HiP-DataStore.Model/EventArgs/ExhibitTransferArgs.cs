df8e07eba95a735aa236b0181ae4e3ee35c79eee
Michél Burkhardt <mburkhardt@live.de>
2018-06-25 14:14:39 +0200

    Fixed questions property not being transfered (iss-hipcms-1045) (#138)
    * Fixed questions property not being transfered
    
    * Fixed R# issues

---
+++ HiP-DataStore.Model/EventArgs/ExhibitTransferArgs.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace PaderbornUniversity.SILab.Hip.DataStore.Model.EventArgs
+{
+    public class ExhibitTransferArgs
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int? Image { get; set; }
+
+        public float Latitude { get; set; }
+
+        public float Longitude { get; set; }
+
+        public ContentStatus Status { get; set; }
+
+        public List<int> Tags { get; set; }
+
+        public List<int> Pages { get; set; }
+
+        public float AccessRadius { get; set; }
+
+        public List<int> Questions { get; set; }
+    }
+}
