77f2f6f75d99d8e5aecb9dcaf4a68c309bfd08d5
Dany Wehbe <dany.wehbe@gmail.com>
2018-08-31 23:04:33 +0200

    initial version of "average of last month rating" is implemented

--- HiP-DataStore/Core/WriteModel/RatingIndex.cs
+++ HiP-DataStore/Core/WriteModel/RatingIndex.cs
@@ -19,7 +19,7 @@ namespace PaderbornUniversity.SILab.Hip.DataStore.Core.WriteModel
         {
             return GetOrCreateRatingTypeInfo(res).Ratings.TryGetValue(id, out var t) ? t.NumberRates : 0;
         }
-        public Dictionary<int,int> Table(ResourceType res, int id)
+        public Dictionary<int, int> Table(ResourceType res, int id)
         {
             return GetOrCreateRatingTypeInfo(res).Ratings.TryGetValue(id, out var t) ? t.GetRatingTable() : null;
         }
@@ -29,15 +29,20 @@ namespace PaderbornUniversity.SILab.Hip.DataStore.Core.WriteModel
             return GetOrCreateRatingTypeInfo(res).Ratings.TryGetValue(id, out var t) ? t.GetUserRating(identity.GetUserIdentity()) : 0;
         }
 
-        public double Average(ResourceType res,int id)
+        public double Average(ResourceType res, int id)
         {
             return GetOrCreateRatingTypeInfo(res).Ratings.TryGetValue(id, out var t) ? t.AverageRate : 0;
         }
 
+        public double LastMonthAverage(ResourceType res, int id)
+        {
+            return GetOrCreateRatingTypeInfo(res).Ratings.TryGetValue(id, out var t) ? t.CalculateLastMonthAverageRating() : 0;
+        }
+
         public int NextId(ResourceType entityType)
         {
-             var info = GetOrCreateRatingTypeInfo(entityType);
-             return ++info.MaximumId;            
+            var info = GetOrCreateRatingTypeInfo(entityType);
+            return ++info.MaximumId;
         }
 
         public void ApplyEvent(IEvent e)
@@ -52,7 +57,8 @@ namespace PaderbornUniversity.SILab.Hip.DataStore.Core.WriteModel
                         ratedType.Ratings.Add(ev.EntityId, new RatingEntityInfo());
 
                     var ratedEntity = ratedType.Ratings[ev.EntityId];
-                    ratedEntity.AddRating(ev.UserId, ev.Value);
+                    //ratedEntity.AddRating(ev.UserId, ev.Value);
+                    ratedEntity.AddRating(ev.UserId, ev.Value, ev.Timestamp);
                     break;
             }
         }
@@ -71,60 +77,82 @@ namespace PaderbornUniversity.SILab.Hip.DataStore.Core.WriteModel
     {
         public int MaximumId { get; set; } = -1;
 
+        //the key is the exhibit index, and the value is an object contains the rating info of the specified exhibit
         public Dictionary<int, RatingEntityInfo> Ratings { get; } = new Dictionary<int, RatingEntityInfo>();
 
     }
+
     class RatingEntityInfo
     {
 
-        public double AverageRate { get { return (NumberRates != 0) ? ((double)_sumRate / NumberRates) : 0; }  }
+        public double AverageRate { get { return (NumberRates != 0) ? ((double)_sumRate / NumberRates) : 0; } }
         public int NumberRates { get; private set; }
 
-        // <UserId, Rating>
-        private Dictionary<string, byte> _allRates = new Dictionary<string, byte>();
+        // <UserId, RatingStruct contains the rate value and its date>
+        private Dictionary<string, RatingDateStruct> _allRates = new Dictionary<string, RatingDateStruct>();
         private int _sumRate;
 
-        public void AddRating(string userId, byte rate)
+        public void AddRating(string userId, byte rate, DateTimeOffset rateDate)
         {
             if (_allRates.TryGetValue(userId, out var oldRate))
             {
-                if(CalculateAverageRating(oldRate, rate))
-                    _allRates[userId] = rate;
+                if (CalculateAverageRating(oldRate.Rate, rate))
+                    _allRates[userId] = new RatingDateStruct { Rate = rate, RateDate = rateDate };
             }
             else
             {
-                if(CalculateAverageRating(null, rate))
-                    _allRates.Add(userId, rate);
+                if (CalculateAverageRating(null, rate))
+                    _allRates.Add(userId, new RatingDateStruct { Rate = rate, RateDate = rateDate });
             }
         }
 
         public byte? GetUserRating(string userId)
         {
-            return _allRates.TryGetValue(userId, out var rating) ? (byte?)rating : null;
+            return _allRates.TryGetValue(userId, out var rating) ? (byte?)rating.Rate : null;
         }
 
-        public Dictionary<int,int> GetRatingTable()
+        public Dictionary<int, int> GetRatingTable()
         {
-            Dictionary <int,int> table = new Dictionary <int,int> { {1,0}, {2, 0}, {3, 0}, {4, 0}, {5, 0} };
-            foreach (var rate in _allRates)
+            Dictionary<int, int> table = new Dictionary<int, int> { { 1, 0 }, { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 } };
+            foreach (var rateStruct in _allRates)
             {
-                table[rate.Value]++;
+                table[rateStruct.Value.Rate]++;
             }
             return table;
         }
+
+        public double CalculateLastMonthAverageRating()
+        {
+            var nowDate = DateTimeOffset.Now;
+            double lastMonthTotalRating = 0, lastMonthAverage = 0;
+            int counter = 0;
+            //iterate through _allRates and calculate the average rating of the last month
+            foreach (var rateStruct in _allRates)
+            {
+                if (rateStruct.Value.RateDate.Month == (nowDate.Month - 1) && rateStruct.Value.RateDate.Year == nowDate.Year)
+                {
+                    counter++;
+                    lastMonthTotalRating += rateStruct.Value.Rate;
+                }
+            }
+            if (counter > 0)
+                lastMonthAverage = lastMonthTotalRating / counter;
+            return lastMonthAverage;
+        }
+
         bool CalculateAverageRating(int? oldRate, int newRate)
         {
             if (!(IsOldRatingValid(oldRate) && CheckRatingRange(newRate)))
                 return false;
 
             if (oldRate != null)
-                  _sumRate += (newRate - oldRate.GetValueOrDefault());
+                _sumRate += (newRate - oldRate.GetValueOrDefault());
             else
             {
                 _sumRate += newRate;
                 NumberRates++;
             }
-            
+
             return true;
         }
         bool IsOldRatingValid(int? oldRate)
@@ -136,5 +164,11 @@ namespace PaderbornUniversity.SILab.Hip.DataStore.Core.WriteModel
             return (rating >= RatingArgs.MinRateValue && rating <= RatingArgs.MaxRateValue);
         }
 
+    }
+
+    struct RatingDateStruct
+    {
+        public byte Rate;
+        public DateTimeOffset RateDate;
     }
 }
